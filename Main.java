b9a70a135156d393922bfccb2edcf7da102897fc
Klyde Garcia <garciaklyde@gmail.com>
2026-09-14 14:31:05 +0800

    new test

--- Main.java
+++ Main.java
@@ -1,17 +1,24 @@
 public class Main {
     public static void main(String[] args) {
 
+
+        Customer klyde = new Customer(100, "Klyde Garcia");
+        Order order = new Order(100, klyde);
+
         MenuItem pizza = new MenuItem(100, "Pizza", 89);
         MenuItem chicken = new MenuItem(101, "Chicken", 150);
         MenuItem cheesecake = new MenuItem(102, "Cheesecake", 59);
 
-        Customer klyde = new Customer(100, "Klyde Garcia");
-        Customer keanne = new Customer(101, "Keanne Garcia");
+        OrderItem orderitem = new OrderItem(pizza, 2);
+        OrderItem orderitem2 = new OrderItem(chicken, 1);
+        OrderItem orderitem3 = new OrderItem(cheesecake, 3);
 
-        klyde.displayInfo();
-        keanne.displayInfo();
+        order.addOrderItem(orderitem);
+        order.addOrderItem(orderitem2);
+        order.addOrderItem(orderitem3);
 
-        Order klydeOrder = new Order(100, klyde);
-        klydeOrder.displayInfo();
+        order.displayInfo();
+
+        System.out.println(order.calculateTotal());
     }
 }
